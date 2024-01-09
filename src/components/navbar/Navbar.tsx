3e816214f7113ce2b34cc5d425512009b01075f0
Henri Remonen <henri@remonen.fi>
2024-01-09 14:53:54 +0200

    feat: update navbar styling

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import DarkModeToggle from "../theme/DarkModeToggle";
 const Navbar = () => {
   return (
     <header>
-      <nav className="px-5vw py-9 lg:py-12">
+      <nav className="px-12 py-4 lg:py-8 text-primary mx-auto flex max-w-8xl items-center justify-between">
         <div className="flex justify-center gap-4 align-middle">
           <a
             className="text-primary underlined block whitespace-nowrap text-2xl font-medium transition focus:outline-none"
--- src/components/theme/DarkModeToggle.tsx
+++ src/components/theme/DarkModeToggle.tsx
@@ -9,7 +9,8 @@ import { RiMoonLine, RiSunLine } from "react-icons/ri";
 const DarkModeToggle = () => {
   const { theme, setTheme } = useTheme();
 
-  const Icon = theme === "light" ? <RiMoonLine /> : <RiSunLine />;
+  const Icon =
+    theme === "light" ? <RiMoonLine size={24} /> : <RiSunLine size={24} />;
 
   return (
     <ToggleButton
