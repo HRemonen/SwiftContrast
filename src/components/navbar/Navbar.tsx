92d56144e8ad7870861df930defacdf21a021d29
Henri Remonen <henri@remonen.fi>
2024-01-10 09:01:13 +0200

    feat: update navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,23 +1,51 @@
+import Link from "next/link";
 import SkipLink from "../common/SkipLink";
 import DarkModeToggle from "../theme/DarkModeToggle";
 
 const Navbar = () => {
+  const NAV_ITEMS = [
+    {
+      name: "Home",
+      href: "/",
+    },
+    {
+      name: "About",
+      href: "/about",
+    },
+    {
+      name: "Tutorial",
+      href: "/tutorial",
+    },
+  ];
+
   return (
-    <header>
+    <header role="banner" className="px-12 py-4 lg:py-8 mx-auto flex max-w-8xl items-center justify-between">
       <SkipLink href="#main"/>
-      <nav className="px-12 py-4 lg:py-8 mx-auto flex max-w-8xl items-center justify-between">
-        <div className="flex justify-center gap-4 align-middle">
-          <a
-            className="block whitespace-nowrap text-3xl font-semibold transition focus:outline-none"
-            href="/"
-          >
-            Swift Contrast
-          </a>
-        </div>
-        <div className="flex items-center justify-center">
-          <DarkModeToggle />
-        </div>
+
+      <div id="logo" className="flex justify-center gap-4 align-middle">
+        <Link
+          className="block whitespace-nowrap text-3xl font-semibold transition focus:outline-none"
+          href="/"
+        >
+          Swift Contrast
+        </Link>
+      </div>
+
+      <nav id="primary-nav" aria-label="Primary" className="hidden lg:block">
+        <ul className="flex">
+          {NAV_ITEMS.map((item) => (
+            <li key={item.name} className="px-5 py-2">
+              <Link href={item.href}>
+                {item.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
       </nav>
+
+      <div className="flex items-center justify-center">
+        <DarkModeToggle />
+      </div>
     </header>
   );
 };
