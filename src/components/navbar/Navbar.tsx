699cd20a9e475087b3e194af181dc64d05942e0d
Henri Remonen <henri@remonen.fi>
2024-01-10 08:39:36 +0200

    feat: update Navbar component

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,15 +1,17 @@
+import SkipLink from "../common/SkipLink";
 import DarkModeToggle from "../theme/DarkModeToggle";
 
 const Navbar = () => {
   return (
     <header>
-      <nav className="px-12 py-4 lg:py-8 text-primary mx-auto flex max-w-8xl items-center justify-between">
+      <SkipLink href="#main"/>
+      <nav className="px-12 py-4 lg:py-8 mx-auto flex max-w-8xl items-center justify-between">
         <div className="flex justify-center gap-4 align-middle">
           <a
-            className="text-primary underlined block whitespace-nowrap text-2xl font-medium transition focus:outline-none"
+            className="block whitespace-nowrap text-3xl font-semibold transition focus:outline-none"
             href="/"
           >
-            <h1>Swift Contrast</h1>
+            Swift Contrast
           </a>
         </div>
         <div className="flex items-center justify-center">
