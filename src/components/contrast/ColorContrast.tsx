03a7fe0f40eb41e3ea45613001302eeb63cd441b
Henri Remonen <henri@remonen.fi>
2024-01-12 14:55:04 +0200

    refactor: extract color related utility functions

--- src/components/contrast/ColorContrast.tsx
+++ src/components/contrast/ColorContrast.tsx
@@ -5,50 +5,9 @@ import React, { useState } from "react";
 import ColorPicker from "./ColorPicker";
 import ColorPreview from "./ColorPreview";
 
-const hexToRgb = (hex: string) => {
-  const hexValue = hex.replace("#", "");
+import { calculateRGBsContrast } from "@/_lib/colors";
 
-  const r = parseInt(hexValue.substring(0, 2), 16);
-  const g = parseInt(hexValue.substring(2, 4), 16);
-  const b = parseInt(hexValue.substring(4, 6), 16);
-
-  return { r, g, b };
-};
-
-const calculateLuminance = (rgb: { r: number; g: number; b: number }) => {
-  const { r, g, b } = rgb;
-
-  const rsrgb = r / 255;
-  const gsrgb = g / 255;
-  const bsrgb = b / 255;
-
-  const rL =
-    rsrgb <= 0.03928 ? rsrgb / 12.92 : ((rsrgb + 0.055) / 1.055) ** 2.4;
-  const gL =
-    gsrgb <= 0.03928 ? gsrgb / 12.92 : ((gsrgb + 0.055) / 1.055) ** 2.4;
-  const bL =
-    bsrgb <= 0.03928 ? bsrgb / 12.92 : ((bsrgb + 0.055) / 1.055) ** 2.4;
-
-  const luminance = 0.2126 * rL + 0.7152 * gL + 0.0722 * bL;
-
-  return luminance;
-};
-
-const calculateRGBsContrast = (text: string, background: string) => {
-  const rgbText = hexToRgb(text);
-  const rgbBackground = hexToRgb(background);
-
-  const luminanceText = calculateLuminance(rgbText);
-  const luminanceBackground = calculateLuminance(rgbBackground);
-
-  const contrast =
-    (Math.max(luminanceText, luminanceBackground) + 0.05) /
-    (Math.min(luminanceText, luminanceBackground) + 0.05);
-
-  return +contrast.toFixed(2);
-};
-
-const calculateWCAGConformance = (contrast: number) => {
+const calculateWCAGCompliance = (contrast: number) => {
   const thresholds = {
     AA: { normal: 4.5, large: 3.0, components: 3.0 },
     AAA: { normal: 7.0, large: 4.5 },
@@ -84,7 +43,7 @@ const generateComplianceColor = (
 
 const ContrastChecker = ({ textColor, backgroundColor }: Colors) => {
   const contrast = calculateRGBsContrast(textColor, backgroundColor);
-  const conformance = calculateWCAGConformance(contrast);
+  const conformance = calculateWCAGCompliance(contrast);
 
   const smallTextStyles = generateComplianceColor(
     conformance.AA.normal,
