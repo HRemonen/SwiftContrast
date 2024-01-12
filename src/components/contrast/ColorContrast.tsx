7160bef2d9664fdd503b9e9381f031bb5ad99f67
Henri Remonen <henri@remonen.fi>
2024-01-12 14:47:31 +0200

    refactor: extract ColorPreview

--- src/components/contrast/ColorContrast.tsx
+++ src/components/contrast/ColorContrast.tsx
@@ -3,20 +3,7 @@
 import React, { useState } from "react";
 
 import ColorPicker from "./ColorPicker";
-
-const ColorPreview = ({ textColor, backgroundColor }: Colors) => (
-  <div
-    className="flex flex-col justify-center items-center mx-2 lg:mx-0 mb-4 md:mb-0 md:order-3 w-full max-w-[600px] min-h-[200px] lg:p-4 border-solid border-[1px] border-gray-400 dark:border-0 rounded-md"
-    style={{ backgroundColor, color: textColor }}
-  >
-    <p className="text-center font-normal text-[24px]">
-      Large text 18pt / 24px
-    </p>
-    <p className="text-center font-normal text-[18.5px]">
-      Small text 14pt / 18.5px
-    </p>
-  </div>
-);
+import ColorPreview from "./ColorPreview";
 
 const hexToRgb = (hex: string) => {
   const hexValue = hex.replace("#", "");
