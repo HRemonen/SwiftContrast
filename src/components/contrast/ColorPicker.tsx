fef9cd22078f76451f69ebabd666278185ee449f
Henri Remonen <henri@remonen.fi>
2024-01-14 12:57:36 +0200

    fix: fix pointer style for input

--- src/components/contrast/ColorPicker.tsx
+++ src/components/contrast/ColorPicker.tsx
@@ -4,6 +4,11 @@ const ColorPickerInput = ({ id, label, color, onChange }: ColorPickerProps) => {
   const textInputId = `${id}-hex-input`;
   const colorInputId = `${id}-picker-input`;
 
+  const handlePasteFromClipboard = async () => {
+    const clipboardText = await navigator.clipboard.readText();
+    onChange(clipboardText);
+  };
+
   return (
     <div className="flex flex-col">
       <div className="flex p-1 w-full md:w-full lg:w-[250px] border-[1px] dark:border-0 border-gray-400 dark:bg-gray-50 dark:bg-opacity-15 rounded-md">
@@ -12,7 +17,7 @@ const ColorPickerInput = ({ id, label, color, onChange }: ColorPickerProps) => {
         </label>
         <input
           className="appearance-none h-10 p-2 mr-2 w-[100%] hover:border-2 border-gray-300 hover:border-blue-600 dark:hover:border-blue-300 rounded-md"
-          style={{ backgroundColor: "transparent", cursor: "pointer" }}
+          style={{ backgroundColor: "transparent" }}
           id={textInputId}
           placeholder="#000000"
           type="text"
