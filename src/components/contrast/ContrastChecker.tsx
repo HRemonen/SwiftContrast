d38491a3338595e1472fae2aa5b0b3272b1a608e
Henri Remonen <henri@remonen.fi>
2024-01-14 10:58:37 +0200

    feat: update the ContrastChecker styles and the information displayed

--- src/components/contrast/ContrastChecker.tsx
+++ src/components/contrast/ContrastChecker.tsx
@@ -8,15 +8,15 @@ const ContrastChecker = ({ textColor, backgroundColor }: Colors) => {
   const contrast = calculateRGBsContrast(textColor, backgroundColor);
   const conformance = calculateWCAGCompliance(contrast);
 
-  const smallTextStyles = generateComplianceColor(
+  const minimumStyles = generateComplianceColor(
     conformance.AA.normal,
-    conformance.AAA.normal,
-  );
-  const largeTextStyles = generateComplianceColor(
     conformance.AA.large,
+  );
+  const enhancedStyles = generateComplianceColor(
+    conformance.AAA.normal,
     conformance.AAA.large,
   );
-  const componentsStyles = generateComplianceColor(
+  const componentStyles = generateComplianceColor(
     conformance.AA.components,
     conformance.AA.components,
   );
@@ -26,32 +26,48 @@ const ContrastChecker = ({ textColor, backgroundColor }: Colors) => {
       <p className="mb-2 lg:mb-4 capitalize whitespace-nowrap text-md lg:text-lg font-semibold text-left">
         Contrast Ratio: {contrast}:1
       </p>
-      <div className="divide-y-2 divide-solid dark:divide-gray-800">
-        <div
-          className="rounded-t-md px-4 pt-4 pb-2"
-          style={{ ...smallTextStyles }}
-        >
-          <p className="capitalize whitespace-nowrap text-md lg:text-lg font-semibold text-left">
-            Small Text 14pt / 18.5px
+
+      <div className="mb-4">
+        <div className="px-4 rounded-sm py-2" style={{ ...minimumStyles }}>
+          <p className="capitalize text-sm md:text-base font-semibold text-left">
+            1.4.3 Contrast (Minimum) - Level AA
           </p>
-          <p>AA: {conformance.AA.normal ? "Pass" : "Fail"}</p>
-          <p>AAA: {conformance.AAA.normal ? "Pass" : "Fail"}</p>
         </div>
-        <div className="px-4 py-2" style={{ ...largeTextStyles }}>
-          <p className="capitalize whitespace-nowrap text-md lg:text-lg font-semibold text-left">
-            Large Text 18pt / 24px
+        <div className="my-2 px-4 text-sm md:flex justify-between">
+          <p>
+            {conformance.AA.normal ? "Pass" : "Fail"} (Small Text 14pt/18.67px)
           </p>
-          <p>AA: {conformance.AA.large ? "Pass" : "Fail"}</p>
-          <p>AAA: {conformance.AAA.large ? "Pass" : "Fail"}</p>
+          <p>{conformance.AA.large ? "Pass" : "Fail"} (Large Text 18pt/24px)</p>
         </div>
-        <div
-          className="rounded-b-md px-4 pt-2 pb-4"
-          style={{ ...componentsStyles }}
-        >
-          <p className="capitalize whitespace-nowrap text-md lg:text-lg font-semibold text-left">
-            UI Components
+      </div>
+
+      <div className="mb-4">
+        <div className="px-4 rounded-sm py-2" style={{ ...enhancedStyles }}>
+          <p className="capitalize text-sm md:text-base font-semibold text-left">
+            1.4.6 Contrast (Enhanced) - Level AAA
           </p>
-          <p>AA: {conformance.AA.components ? "Pass" : "Fail"}</p>
+        </div>
+        <div className="my-2 px-4 text-sm md:flex justify-between">
+          <p>
+            {conformance.AAA.normal ? "Pass" : "Fail"} (Small Text 14pt/18.67px)
+          </p>
+          <p>
+            {conformance.AAA.large ? "Pass" : "Fail"} (Large Text 18pt/24px)
+          </p>
+        </div>
+      </div>
+
+      <div>
+        <div className="px-4 rounded-sm py-2" style={{ ...componentStyles }}>
+          <p className="capitalize text-sm md:text-base font-semibold text-left">
+            1.4.11 Non-text Contrast - Level AA
+          </p>
+        </div>
+        <div className="my-2 px-4 text-sm">
+          <p>
+            {conformance.AA.components ? "Pass" : "Fail"} (UI Components and
+            Graphical Objects)
+          </p>
         </div>
       </div>
     </div>
