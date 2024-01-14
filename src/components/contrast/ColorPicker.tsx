05b69dc3f8fe6568ee1183011b352456a7b4d695
Henri Remonen <henri@remonen.fi>
2024-01-14 12:53:26 +0200

    feat: add handler for copying colors

--- src/components/contrast/ColorPicker.tsx
+++ src/components/contrast/ColorPicker.tsx
@@ -37,18 +37,32 @@ const ColorPickerInput = ({ id, label, color, onChange }: ColorPickerProps) => {
   );
 };
 
-const ColorPicker = ({ id, label, color, onChange }: ColorPickerProps) => (
-  <div className="mx-2 lg:mx-0 mb-2 lg:mb-8">
-    <div className="flex justify-between items-start">
-      <h2 className="mb-2 lg:mb-4 block capitalize text-md lg:text-lg font-semibold text-left">
-        {label}
-      </h2>
-      <button className="mr-4 md:mr-3 hover:cursor-pointer hover:text-blue-600 dark:hover:text-blue-300">
-        <IoCopyOutline size={24} />
-      </button>
+const ColorPicker = ({ id, label, color, onChange }: ColorPickerProps) => {
+  const handleCopyToClipboard = () => {
+    navigator.clipboard.writeText(color);
+  };
+
+  return (
+    <div className="mx-2 lg:mx-0 mb-2 lg:mb-8">
+      <div className="flex justify-between items-start">
+        <h2 className="mb-2 lg:mb-4 block capitalize text-md lg:text-lg font-semibold text-left">
+          {label}
+        </h2>
+        <button
+          className="mr-4 md:mr-3 hover:cursor-pointer hover:text-blue-600 dark:hover:text-blue-300"
+          onClick={handleCopyToClipboard}
+        >
+          <IoCopyOutline size={24} />
+        </button>
+      </div>
+      <ColorPickerInput
+        id={id}
+        label={label}
+        color={color}
+        onChange={onChange}
+      />
     </div>
-    <ColorPickerInput id={id} label={label} color={color} onChange={onChange} />
-  </div>
-);
+  );
+};
 
 export default ColorPicker;
