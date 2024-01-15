498b9ac2c762380007866499a40a1fb942e8f6b5
Henri Remonen <henri@remonen.fi>
2024-01-15 16:32:20 +0200

    build: vercel integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import ContrastCheckerGuide from "@/components/guides/ContrastCheckerGuide";
 export default function Home() {
   return (
     <section id="home">
+      <title>Swift Contrast - WCAG Contrast Checker</title>
       <h1 className="block whitespace-nowrap text-xl lg:text-3xl font-semibold text-center">
         Swift Contrast
       </h1>
