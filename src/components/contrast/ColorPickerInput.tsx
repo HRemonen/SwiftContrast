324ce5cb730f009525462c7c05d7b8c4e5a4f830
Henri Remonen <henri@remonen.fi>
2024-02-04 16:43:54 +0200

    style: indicate invalid input on hex field

--- src/components/contrast/ColorPickerInput.tsx
+++ src/components/contrast/ColorPickerInput.tsx
@@ -6,17 +6,19 @@ const ColorPickerInput = ({ id, label, color, onChange }: ColorPickerProps) => {
 
   return (
     <div className='flex flex-col'>
-      <div className='flex w-full rounded-md border-[1px] border-gray-400 p-1 dark:border-0 dark:bg-gray-50 dark:bg-opacity-15 md:w-full'>
+      <div className='flex w-full rounded-md border-[1px] border-gray-400 p-1 dark:border-0 dark:bg-gray-50 dark:bg-opacity-15 md:w-full has-[input:invalid]:border-red-500 dark:has-[input:invalid]:border-[1px] dark:has-[input:invalid]:border-red-200'>
         <label className='sr-only' htmlFor={textInputId}>
           {label} HEX
         </label>
         <input
-          className='mr-2 h-10 w-[100%] appearance-none rounded-md border-gray-300 p-2 hover:border-2 hover:border-blue-600 dark:hover:border-blue-300'
+          className='mr-2 h-10 w-[100%] appearance-none rounded-md border-gray-300 p-2 hover:border-2 hover:border-blue-600 dark:hover:border-blue-300 invalid:text-red-500 dark:invalid:text-red-200'
           style={{ backgroundColor: 'transparent' }}
           id={textInputId}
-          placeholder='#000000'
           type='text'
           value={color}
+          placeholder='#000000'
+          pattern="#[0-9a-fA-F]{3}([0-9a-fA-F]{3})?"
+          required
           onChange={(e) => onChange(e.target.value)}
         />
 
@@ -27,9 +29,9 @@ const ColorPickerInput = ({ id, label, color, onChange }: ColorPickerProps) => {
           className='min-h-10 min-w-10 rounded-md hover:border-2 hover:border-blue-600 dark:hover:border-blue-300'
           style={{ backgroundColor: 'transparent', cursor: 'pointer' }}
           id={colorInputId}
-          placeholder='#ffffff'
           type='color'
           value={color}
+          placeholder='#ffffff'
           onChange={(e) => onChange(e.target.value)}
         />
       </div>
