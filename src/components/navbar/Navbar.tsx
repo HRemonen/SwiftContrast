8bc377f0b7994180447353e30edb6cc60b02a4db
Henri Remonen <henri@remonen.fi>
2024-03-30 11:59:13 +0200

    feat: removed navbar elements that are not in use currently

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,4 +1,3 @@
-import Link from 'next/link'
 import dynamic from 'next/dynamic'
 import SkipLink from '../common/SkipLink'
 
@@ -6,39 +5,23 @@ const DarkModeToggle = dynamic(() => import('../theme/DarkModeToggle'), {
   ssr: false,
 })
 
-const Navbar = () => {
-  // This is for further possible development
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const NAV_ITEMS: any[] = []
+const Navbar = () => (
+  <header
+    role='banner'
+    className='mx-auto flex items-center justify-between px-4 py-4 md:px-8 md:py-8 lg:px-12 lg:py-8'
+  >
+    <SkipLink href='#main' />
 
-  return (
-    <header
-      role='banner'
-      className='mx-auto flex items-center justify-between px-4 py-4 md:px-8 md:py-8 lg:px-12 lg:py-8'
-    >
-      <SkipLink href='#main' />
+    <div id='logo' className='flex justify-center gap-4 align-middle'>
+      <p className='block whitespace-nowrap text-lg font-semibold transition focus:outline-none md:text-xl lg:text-3xl'>
+        Swift Contrast
+      </p>
+    </div>
 
-      <div id='logo' className='flex justify-center gap-4 align-middle'>
-        <p className='block whitespace-nowrap text-lg font-semibold transition focus:outline-none md:text-xl lg:text-3xl'>
-          Swift Contrast
-        </p>
-      </div>
-
-      <nav id='primary-nav' aria-label='Primary' className='hidden lg:block'>
-        <ul className='flex'>
-          {NAV_ITEMS.map((item) => (
-            <li key={item.name} className='px-5 py-2'>
-              <Link href={item.href}>{item.name}</Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-
-      <div className='flex items-center justify-center'>
-        <DarkModeToggle />
-      </div>
-    </header>
-  )
-}
+    <div className='flex items-center justify-center'>
+      <DarkModeToggle />
+    </div>
+  </header>
+)
 
 export default Navbar
