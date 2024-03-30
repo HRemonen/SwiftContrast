83e4ba98173ca608f8443b28dfcc8ff42413ab32
Henri Remonen <henri@remonen.fi>
2024-03-30 15:16:50 +0200

    style: better skip link

--- src/components/common/SkipLink.tsx
+++ src/components/common/SkipLink.tsx
@@ -3,7 +3,10 @@ interface SkipLinkProps {
 }
 
 const SkipLink = ({ href }: SkipLinkProps) => (
-  <a href={href} className='sr-only'>
+  <a
+    href={href}
+    className='sr-only translate-y-[-150%] transform focus:not-sr-only focus:absolute focus:left-1/2 focus:translate-y-0 focus:transition-transform '
+  >
     Skip to content
   </a>
 )
